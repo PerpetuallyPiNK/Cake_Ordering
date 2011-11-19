d1d6bb053ac0ba6b6938671071141ea9fd1f661d
Angela Shatashwili <ashatashwili@yahoo.com>
2011-11-19 00:44:23 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CakeOrdering::Application.config.secret_token = '35c9d6fb294bea09ac899510fe0d837583141ac27614e5bb16c19f0e668cedf09bcd28e7c97651bf93bab55d0bd11e98ccf41b9f7f5d11412e80a457e741e642'
