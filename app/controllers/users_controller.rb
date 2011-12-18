67198e744289e1724e9a6bf663818ce3d1a3ab38
Angela Shatashwili <ashatashwili@yahoo.com>
2011-12-18 18:38:24 -0500

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,27 @@ class UsersController < ApplicationController
   end	
 	
   def new
+    @user = User.new
     @title = "Sign Up"
   end
   
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to DOODLED!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
