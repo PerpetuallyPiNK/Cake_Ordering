47a3580abb828324b021046e6217aa1a96234885
Angela Shatashwili <ashatashwili@yahoo.com>
2011-12-19 11:06:51 -0500

    Revert "Checked files with reference"
    This reverts commit cc62d17a13ae9ba4cb6694cab10e8c0f63e424dc. Revert.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,8 @@
 class SessionsController < ApplicationController
-
   def new
     @title = "Sign in"
   end
-  
+
   def create
     user = User.authenticate(params[:session][:email],
                              params[:session][:password])
@@ -16,7 +15,7 @@ class SessionsController < ApplicationController
       redirect_back_or user
     end
   end
-  
+
   def destroy
     sign_out
     redirect_to root_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,75 +2,87 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
-  
+ 
   def index
+    @title = "All Doodlers!"
     @users = User.paginate(:page => params[:page])
-    @title = "All users"
   end
-  
+
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
-  end
-
-  def following
-    @title = "Following"
-    @user = User.find(params[:id])
-    @users = @user.following.paginate(:page => params[:page])
-    render 'show_follow'
-  end
-  
-  def followers
-    @title = "Followers"
-    @user = User.find(params[:id])
-    @users = @user.followers.paginate(:page => params[:page])
-    render 'show_follow'
-  end
-
+  end	
+	
   def new
-    @user  = User.new
-    @title = "Sign up"
+    @user = User.new
+    @title = "Sign Up"
   end
   
   def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       @title = "Sign up"
       render 'new'
     end
   end
   
-  def edit
+    def edit
     @title = "Edit user"
   end
   
-  def update
+   def update
+    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      redirect_to @user, :flash => { :success => "Profile updated." }
+      flash[:success] = "Profile updated."
+      redirect_to @user
     else
       @title = "Edit user"
       render 'edit'
     end
+   end
+   
+   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
+    def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
   end
 
-  def destroy
-    @user.destroy
-    redirect_to users_path, :flash => { :success => "User destroyed." }
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
   end
+   
+     private
 
-  private
+#    def authenticate
+#      deny_access unless signed_in?
+#    end
 
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-    
-    def admin_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) if !current_user.admin? || current_user?(@user)
+	
+	  def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
+	
+	  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
+    @title = @user.name
+  end
 end
